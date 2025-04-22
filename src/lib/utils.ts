87b99bb945a9bc46c998f9e75c244ec48f5d13a9
Razvan M. <76774976+iRazvan2745@users.noreply.github.com>
2025-04-23 00:44:21 +0300

    add formatbits function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -19,6 +19,20 @@ export function formatBytes(bytes: number, decimals = 2): string {
   return `${Number.parseFloat((bytes / Math.pow(k, i)).toFixed(dm))} ${sizes[i]}`
 }
 
+export function formatBits(bits: number, decimals = 2): string {
+  if (!+bits) {
+    return "0 Bits"
+  }
+
+  const k = 1000
+  const dm = decimals < 0 ? 0 : decimals
+  const sizes = ["Bits", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"]
+
+  const i = Math.floor(Math.log(bits) / Math.log(k))
+
+  return `${Number.parseFloat((bits / Math.pow(k, i)).toFixed(dm))} ${sizes[i]}`
+}
+
 export function calculatePercentage(available: number, total: number): number {
   if (!total) return 0;
   return Math.round((available / total) * 100)
